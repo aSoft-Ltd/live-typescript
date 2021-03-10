d75574dfa15742bac58f487c55da6baed1162c81
andylamax <andylamax@programmer.net>
2021-03-10 10:34:07 +0300

    Updated git ignore

--- live-core/dist/test/tools.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.delay = void 0;
-function delay(time) {
-    return new Promise(function (res) {
-        setTimeout(function () { res(); }, time);
-    });
-}
-exports.delay = delay;
